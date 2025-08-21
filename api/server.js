b542de744fb80024bdba81397c88d3016ac5a22f
shrey294 <gshrey470@gmail.com>
2025-08-21 15:50:16 +0530

    remove file

--- api/server.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-
-module.exports = async (req, res) => {
-  const { default: app } = await import(
-    path.join(process.cwd(), "dist/app-porfolio/server/server.mjs")
-  );
-  return app(req, res);
-};
